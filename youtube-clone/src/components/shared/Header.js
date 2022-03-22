ca1c356c352e35d680bb3a11df3bececb95d9113
yoonchairyeon <chryis14@skuniv.ac.kr>
2022-03-23 01:48:57 +0900

    head commit

--- youtube-clone/src/components/shared/Header.js
+++ youtube-clone/src/components/shared/Header.js
@@ -1,28 +1,43 @@
-import styles from "./Header.module.css";
-import youtube_logo from "../../data/youtube_logo.png";
-import {FiMenu} from "react-icons/fi";
-import {IoSearchOutline} from "react-icons/io5";
-import {BsGrid3X3Gap} from "react-icons/bs"
-import {HiOutlineDotsVertical} from "react-icons/hi";
+import styles from './Header.module.css';
+import youtube_logo from '../../data/youtube_logo.png'
+import { FiMenu } from 'react-icons/fi';
+import { IoSearchOutline } from 'react-icons/io5';
+import { BsGrid3X3Gap } from 'react-icons/bs';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
+import { useState } from 'react';
 
-function Header({onClickMenu}) {
-    
-  return (
-    <div className = {styles.header}>
-            <div className = {styles.tab}>
-                <FiMenu className={styles.icon} onClick={onClickMenu}/>
-                <img src={youtube_logo} alt="로고" className={styles.logo}/>
+
+function Header({onClick}) {
+    const [value, setValue] = useState('');
+    function onClick() {
+        console.log(value);
+        setValue('');
+    }
+
+
+    function onChange(event) {
+        setValue(event.target.value);
+    }
+
+
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.tab}>
+                <FiMenu className={styles.icon} onClick={onClick} />
+                <img src={youtube_logo} alt="로고" className={styles.logo} />
             </div>
             <div className={styles['center-tab']}>
-                <input className={styles.input}/>
-                <IoSearchOutline className = {styles['search-icon']}/>
+                <input className={styles.input} onChange={onChange} value={value} />
+                <IoSearchOutline className={styles['search-icon']} onClick={onClick} />
             </div>
             <div className={styles.tab}>
-                <BsGrid3X3Gap className={styles.icon}/>
-                <HiOutlineDotsVertical className={styles.icon}/>
+                <BsGrid3X3Gap className={styles.icon} />
+                <HiOutlineDotsVertical className={styles.icon} />
             </div>
-    </div>
-  );
+        </div>
+    );
 }
+
 
 export default Header;
